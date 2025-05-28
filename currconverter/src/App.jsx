b220cec4bc49b7c52cf8e7c7a9e3d984225b8923
Developer-Madhu <madhuatomix@gmail.com>
2025-05-28 14:00:00 +0530

    imported useId for html label and fields

--- currconverter/src/App.jsx
+++ currconverter/src/App.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [val, setVal] = useState(null)
+  const [val, setVal] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convAmt, setConvAmt] = useState(0)
 
   return (
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url('https://images.pexels.com/photos/8370391/pexels-photo-8370391.jpeg?auto=compress&cs=tinysrgb&w=600)`}}>
--- currconverter/src/Components/InputBox.jsx
+++ currconverter/src/Components/InputBox.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {useId} from 'react'
 
 const InputBox = ({ label, amount, onAmountChange, onCurrChange, currOptions = [], selectedCurr = 'usd', disableAmt = false, currencyDisabled = false, className = '' }) => {
+  const id = useId()
   return (
     <div className={`${className}`}>
       <div className='w-1/2'>
-        <label className='text-black/40 mb-2 inline-block'>{label}</label>
-        <input type="number" className='outline-none w-full bg-transparent py-1.5' placeholder='Amount' disabled={disableAmt} value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
+        <label htmlFor={id} className='text-black/40 mb-2 inline-block'>{label}</label>
+        <input id={id} type="number" className='outline-none w-full bg-transparent py-1.5' placeholder='Amount' disabled={disableAmt} value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
       </div>
       <div className='w-1/2 flex flex-wrap justify-end text-right'>
         <p className='text-black/40 mb-2 w-full'>Currency Type</p>
