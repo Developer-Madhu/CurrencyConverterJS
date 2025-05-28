62b6d49b64b31628e7411b3aa460ad7541997ab3
Developer-Madhu <madhuatomix@gmail.com>
2025-05-28 14:07:42 +0530

    created funtions to swap values & convert amount

--- currconverter/src/App.jsx
+++ currconverter/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 const App = () => {
 
@@ -6,6 +7,18 @@ const App = () => {
   const [from, setFrom] = useState('usd')
   const [to, setTo] = useState('inr')
   const [convAmt, setConvAmt] = useState(0)
+
+  const currInfo = useCurrencyInfo(from)
+  const options = Object.keys(currInfo)
+
+  function swapVals(){
+    setFrom(to)
+    setTo(from)
+  }
+
+  const convert = () => {
+    setConvAmt(val * currInfo[to])
+  }
 
   return (
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url('https://images.pexels.com/photos/8370391/pexels-photo-8370391.jpeg?auto=compress&cs=tinysrgb&w=600)`}}>
